88254f7ae7d2cc1c98486bf388ea5faf8c509c87
rkuczer <57370393+rkuczer@users.noreply.github.com>
2023-03-29 11:50:51 -0400

    Finished, now have output to a json file

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse, abort
+import json
 
 app = Flask(__name__)
 api = Api(app)
 parser = reqparse.RequestParser()
 parser.add_argument('title', required=True)
+parser.add_argument('uploadDate', type=int, required=False)
 
-videos = {
-    'video1': {'title': 'Tequila, Lime Juice, Cointreau'},
-    'video2': {'title': 'Bourbon, Angostura Bitters, Sugar, Orange Peel'},
-    'video3': {'title': 'Rum, Mint, Lime Juice, Sugar, Club Soda'}
-}
-
+with open('videos.json', 'r') as f:
+    videos = json.load(f)
+def write_changes_to_file():
+    global videos
+    videos = {k: v for k, v in sorted(videos.items(), key=lambda video: video[1]['uploadDate'])}
+    with open('videos.json', 'w') as file:
+        json.dump(videos, file)
 
 class Video(Resource):
     def get(self, videoID):
@@ -19,33 +22,39 @@ class Video(Resource):
             return videos
         if videoID not in videos:
             abort(404, message=f"Video {videoID} not found.")
-
         return videos[videoID], 201
 
     def put(self, videoID):
         args = parser.parse_args()
-        new_video = {'title': args['title']}
+        new_video = {'title': args['title'], 'uploadDate': args['uploadDate']}
         videos[videoID] = new_video
-        return {videoID: videos[videoID]}, 201 #good status code
+        write_changes_to_file()
+        return {videoID: videos[videoID]}, 201  # good status code
 
     def delete(self, videoID):
         if videoID not in videos:
             abort(404, message=f"Video {videoID} not found.")
         del videos[videoID]
+        write_changes_to_file()
         return " ", 204
+
 
 class VideoSchedule(Resource):
     def get(self):
         return videos
+
     def post(self):
         args = parser.parse_args()
-        new_video = {'title' : args['title']}
+        new_video = {'title': args['title'], 'uploadDate': args['uploadDate']}
         videoID = max(int(v.lstrip('video')) for v in videos.keys()) + 1
+        videoID = f"video{videoID}"
         videos[videoID] = new_video
+        write_changes_to_file()
         return videos[videoID], 201
 
-api.add_resource(VideoSchedule, '/videos ')
+
 api.add_resource(Video, '/videos/<videoID>')
+api.add_resource(VideoSchedule, '/videos ')
 
 if __name__ == '__main__':
     app.run()
